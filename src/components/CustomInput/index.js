7be8fa30828e5728fb434c2b8215bb34e588ed49
Pham Thi Thuy Trang <trang@192.168.1.7>
2024-04-17 15:37:59 +0700

    NewTaskScreen applied redux

--- src/components/CustomInput/index.js
+++ src/components/CustomInput/index.js
@@ -1,20 +1,33 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { View, TextInput, Image } from 'react-native';
-import styles from './styles'
+import styles from './styles';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const CustomInput = ({ onChangeText, value, placeholder, secureTextEntry, leftIcon, rightIcon,  customInputTextStyle }) => {
+const CustomInput = ({ onChangeText, value, placeholder, secureTextEntry, leftIcon, rightIcon,  customInputTextStyle, showDateTimePicker, onDateTimeChange }) => {
     return (
         <View>
             <View style={[styles.inputContainer]}>
                 <Image source={leftIcon} style={styles.icon} />
-                <TextInput 
-                    value={value}
-                    onChangeText={onChangeText}
-                    placeholder={placeholder}
-                    style={[styles.inputText, customInputTextStyle]}
-                    secureTextEntry={secureTextEntry}>
-                </TextInput>
+                {showDateTimePicker ? (
+                    <DateTimePicker
+                        value={value ? new Date(value) : new Date()}
+                        mode="datetime"
+                        is24Hour={true}
+                        display="default"
+                        onChange={(event, selectedDate) => {
+                            onDateTimeChange(selectedDate);
+                        }}
+                    />
+                ) : (
+                    <TextInput 
+                        value={value}
+                        onChangeText={onChangeText}
+                        placeholder={placeholder}
+                        style={[styles.inputText, customInputTextStyle]}
+                        secureTextEntry={secureTextEntry}
+                    />
+                )}
                 <Image source={rightIcon} style={styles.icon} />
             </View>
         </View>
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -7,7 +7,8 @@ const taskSlice = createSlice({
     weeklyTasks: [],
     monthlyTasks: [],
     laterTasks: [],
-    doneTasks: []
+    doneTasks: [],
+    todoList: [],
   },
   reducers: {
     get_daily_tasks_success: (state, action) => {
@@ -29,10 +30,13 @@ const taskSlice = createSlice({
       const task = action.payload;
       state.laterTasks.push(task);
       state.dailyTasks = state.dailyTasks.filter(item => item.id !== task.id);
+    },
+    createNewTask: (state, action) => {
+      state.dailyTasks.push(action.payload);
     }
   },
 })
 
-export const { get_daily_tasks_success, get_weekly_tasks_success, get_monthly_tasks_success, markTaskDone, markTaskLater } = taskSlice.actions
+export const { get_daily_tasks_success, get_weekly_tasks_success, get_monthly_tasks_success, markTaskDone, markTaskLater, createNewTask } = taskSlice.actions
 
 export default taskSlice.reducer
