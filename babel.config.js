f1d4849b0d993bbbf9ce37835755945c1e62e926
Trang Pham <trang.pham@workforceoptimizer.com>
2025-05-14 14:19:38 +0700

    config env file

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,15 @@ module.exports = {
         },
       }
     ],
-    'react-native-reanimated/plugin'
+    'react-native-reanimated/plugin',
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+      "blacklist": null,
+      "whitelist": null,
+      "safe": false,
+      "allowUndefined": true
+    }]
   ],
   presets: ["module:metro-react-native-babel-preset"]
 };
